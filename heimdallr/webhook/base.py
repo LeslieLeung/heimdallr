06ac7678d6d37bb92fd7b59246e7d608326be7d0
Leslie Leung <lesily9@gmail.com>
2023-03-29 02:52:18 +0800

    fix: GitHub webhook parse

--- heimdallr/webhook/base.py
+++ heimdallr/webhook/base.py
@@ -7,7 +7,7 @@ class WebhookBase:
     def __init__(self, message: Any):
         self.message = message
 
-    def parse(self) -> (str, str):
+    def parse(self) -> (str, str, str):
         """
         Parse the message to be sent.
         return: (title, body)
